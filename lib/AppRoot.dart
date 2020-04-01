57fde5ceb25dffc55aab1ef5758f47d33a50dc09
MathDodo <mathias.h.larsen@jp.dk>
2020-04-01 11:38:41 +0200

    Hello

---
+++ lib/AppRoot.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppRoot extends StatelessWidget
+{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+        home: Text("Hello!")
+    );
+  }
+}
